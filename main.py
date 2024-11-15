f4b56e63b9475f621749d971372a58b7974020a8
Tiket-s8d <timofei.nechaev@gmail.com>
2024-11-15 01:52:34 -0500

    add healthy page

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
-# app.py
 
-from flask import Flask
+from flask import Flask, jsonify
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
     return "Hello, Flask!\n tselthskdfjgdlgjdlkfg\n jgdfngljkdsfnhksrjlthystjlghlkfgh\n"
+
+@app.route('/healthy')
+def healthy():
+    # Возвращает JSON-ответ с сообщением и статусом 200
+    return jsonify(status="healthy", message="Server is healthy!"), 200
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=5000, debug=True)
